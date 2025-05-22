c189ceea1b0d47b542b9b630089d1028c4b2628c
Budi Syahiddin <me@inve.rs>
2025-05-23 04:14:36 +0800

    feat: syntax highlighting

--- markdown-renderer/src/lib.rs
+++ markdown-renderer/src/lib.rs
@@ -1,11 +1,49 @@
-use pulldown_cmark::{Options, Parser};
+use std::sync::LazyLock;
+
+use pulldown_cmark::{CodeBlockKind, Event, Parser, Tag, TagEnd};
+use syntect::highlighting::{Theme, ThemeSet};
+use syntect::html::highlighted_html_for_string;
+use syntect::parsing::SyntaxSet;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn render_md(raw_markdown: &str) -> String {
-    let parser = Parser::new_ext(raw_markdown, Options::all());
-    let mut buf = String::new();
-    let _ = pulldown_cmark::html::write_html_fmt(&mut buf, parser);
+pub fn render_md(markdown: &str) -> String {
+    static SYNTAX_SET: LazyLock<SyntaxSet> = LazyLock::new(SyntaxSet::load_defaults_newlines);
+    static THEME: LazyLock<Theme> = LazyLock::new(|| {
+        let theme_set = ThemeSet::load_defaults();
+        theme_set.themes["base16-ocean.dark"].clone()
+    });
 
-    buf
+    let mut sr = SYNTAX_SET.find_syntax_plain_text();
+    let mut code = String::new();
+    let mut code_block = false;
+    let parser = Parser::new(markdown).filter_map(|event| match event {
+        Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(lang))) => {
+            let lang = lang.trim();
+            sr = SYNTAX_SET
+                .find_syntax_by_token(&lang)
+                .unwrap_or_else(|| SYNTAX_SET.find_syntax_plain_text());
+            code_block = true;
+            None
+        }
+        Event::End(TagEnd::CodeBlock) => {
+            let html = highlighted_html_for_string(&code, &SYNTAX_SET, &sr, &THEME)
+                .unwrap_or(code.clone());
+            code.clear();
+            code_block = false;
+            Some(Event::Html(html.into()))
+        }
+
+        Event::Text(t) => {
+            if code_block {
+                code.push_str(&t);
+                return None;
+            }
+            Some(Event::Text(t))
+        }
+        _ => Some(event),
+    });
+    let mut html_output = String::new();
+    pulldown_cmark::html::push_html(&mut html_output, parser);
+    html_output
 }
