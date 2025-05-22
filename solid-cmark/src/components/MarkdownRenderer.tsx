ffa55e7a07ed6b5b3b29b438092f47536c35b5b8
Budi Syahiddin <me@inve.rs>
2025-05-23 03:54:43 +0800

    feat: remove input box and only show renderer

--- solid-cmark/src/components/MarkdownRenderer.tsx
+++ solid-cmark/src/components/MarkdownRenderer.tsx
@@ -2,16 +2,13 @@ import { type Component, createEffect, createSignal, onMount } from 'solid-js';
 import init, { render_md } from "markdown-renderer";
 
 export interface MarkdownRendererProps {
-  debounce: number
+  markdown: string;
+  class?: string;
 }
 
 export const MarkdownRenderer: Component<MarkdownRendererProps> = (props) => {
-  const { debounce } = props;
-
-  const [markdownInput, setMarkdownInput] = createSignal('');
   const [renderedHtml, setRenderedHtml] = createSignal('');
   const [loadingWasm, setLoadingWasm] = createSignal(true);
-  const [renderLoading, setRenderLoading] = createSignal(false);
   const [error, setError] = createSignal<string | null>(null);
 
   // Initialize Wasmer SDK and compile the WASM module
@@ -24,50 +21,22 @@ export const MarkdownRenderer: Component<MarkdownRendererProps> = (props) => {
       setError(`Failed to initialize WASM: ${err}`);
     } finally {
       setLoadingWasm(false);
+      console.debug("Loaded WASM successfully");
     }
   });
 
-  // Debounced rendering logic
   createEffect(() => {
-    const markdown = markdownInput();
-    if (!loadingWasm()) {
-      const timer = setTimeout(async () => {
-        setRenderLoading(true);
-        setError(null);
-        try {
-          const result = render_md(markdown);
-          setRenderedHtml(result);
-        } catch (err) {
-          console.error("Error rendering Markdown:", err);
-          setError(`Failed to render Markdown: ${err}`);
-          setRenderedHtml('');
-        } finally {
-          setRenderLoading(false);
-        }
-      }, debounce); // Debounce delay
-
-      // Cleanup function to clear the timeout if the component unmounts or input changes quickly
-      return () => clearTimeout(timer);
+    if (!loadingWasm()) {      
+      const result = render_md(props.markdown);
+      setRenderedHtml(result);
     }
   });
 
   return (
     <div>
       {error() && <div style={{ color: 'red' }}>{error()}</div>}
-      {loadingWasm() && <div>Loading WASM...</div>}
-      <div>
-        <label for="markdown-input">Markdown Input:</label><br />
-        <textarea
-          id="markdown-input"
-          value={markdownInput()}
-          onInput={(e) => setMarkdownInput(e.currentTarget.value)}
-          rows={10}
-          cols={80}
-        />
-      </div>
-      <div>
-        <h2>Rendered HTML:</h2>
-        {renderLoading() && <div>Rendering...</div>}
+      {loadingWasm() && <p>Loading WASM...</p>}
+      <div class={props.class}>        
         <div innerHTML={renderedHtml()} />
       </div>
     </div>
