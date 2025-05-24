ce326ed4322d50e1d823a3f7e9f4ec281fc4355e
Budi Syahiddin <me@inve.rs>
2025-05-25 07:51:15 +0800

    feat: remove syntax highlighting for now

--- markdown-renderer/src/lib.rs
+++ markdown-renderer/src/lib.rs
@@ -1,56 +1,24 @@
 use std::sync::LazyLock;
 
-use pulldown_cmark::{CodeBlockKind, Event, Options, Parser, Tag, TagEnd};
-use syntect::highlighting::ThemeSet;
-use syntect::html::highlighted_html_for_string;
-use syntect::parsing::SyntaxSet;
+use comrak::{
+    Options, Plugins, markdown_to_html, markdown_to_html_with_plugins,
+    plugins::syntect::SyntectAdapterBuilder,
+};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn render_md(markdown: &str, theme: &str) -> String {
-    static SYNTAX_SET: LazyLock<SyntaxSet> = LazyLock::new(SyntaxSet::load_defaults_newlines);
-    static THEME_SET: LazyLock<ThemeSet> = LazyLock::new(ThemeSet::load_defaults);
-
-    let theme = THEME_SET.themes[theme].clone();
-
-    let mut sr = SYNTAX_SET.find_syntax_plain_text();
-    let mut code = String::new();
-    let mut html_output = String::new();
-    let mut code_block = false;
-
-    let parser = Parser::new_ext(&markdown, Options::all()).filter_map(|event| match event {
-        Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(lang))) => {
-            let lang = lang.trim();
-            sr = SYNTAX_SET
-                .find_syntax_by_token(&lang)
-                .unwrap_or_else(|| SYNTAX_SET.find_syntax_plain_text());
-            code_block = true;
-            None
-        }
-        Event::End(TagEnd::CodeBlock) => {
-            let html = highlighted_html_for_string(&code, &SYNTAX_SET, &sr, &theme)
-                .unwrap_or(code.clone());
-
-            code.clear();
-            code_block = false;
-            Some(Event::Html(html.into()))
-        }
-        Event::Text(t) => {
-            if code_block {
-                code.push_str(&t);
-                return None;
-            }
-            let sanitized = ammonia::clean(&t);
-            Some(Event::Text(sanitized.into()))
-        }
-        Event::Html(html) => {
-            let sanitized = ammonia::clean(&html);
-            Some(Event::Html(sanitized.into()))
-        }
-        _ => Some(event),
+    static OPTIONS: LazyLock<Options> = LazyLock::new(|| {
+        let mut options = Options::default();
+        options.extension.table = true;
+        options.extension.front_matter_delimiter = Some("---".into());
+        options
     });
 
-    pulldown_cmark::html::push_html(&mut html_output, parser);
+    // let adapter = SyntectAdapterBuilder::new().theme(theme).build();
+    // let mut plugins = Plugins::default();
+    // plugins.render.codefence_syntax_highlighter = Some(&adapter);
 
-    html_output
+    // markdown_to_html_with_plugins(markdown, &OPTIONS, &plugins)
+    markdown_to_html(markdown, &OPTIONS)
 }
