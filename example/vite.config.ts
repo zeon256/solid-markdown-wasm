5e78480e88fbaf735d3c41d8e3010c10b74aba9e
Budi Syahiddin <me@inve.rs>
2025-05-27 04:14:38 +0800

    docs: update github pages and add CNAME

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,19 +3,19 @@ import solid from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [solid(), tailwindcss()],
-	resolve: {
-		dedupe: ["solid-js"],
-	},
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes("node_modules/solid-monaco")) {
-						return "solid-monaco";
-					}
-				},
-			},
-		},
-	},
+  plugins: [solid(), tailwindcss()],
+  resolve: {
+    dedupe: ["solid-js"],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/solid-monaco")) {
+            return "solid-monaco";
+          }
+        },
+      },
+    },
+  },
 });
