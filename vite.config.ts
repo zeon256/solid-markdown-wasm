18989744d8d5640d2077136cab7aeeff25afd521
Budi Syahiddin <me@inve.rs>
2025-05-26 22:44:33 +0800

    build: add compiling of rust code in vite as well as chunking it separately

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,47 @@ import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 import dts from 'vite-plugin-dts'
 import { resolve } from "path";
+import { exec } from "node:child_process";
 
 export default defineConfig({
-  plugins: [solid(), dts({ insertTypesEntry: true, include: ['src'], tsconfigPath: './tsconfig.app.json'}) ],
+  plugins: [solid(), dts({ insertTypesEntry: true, include: ['src'], tsconfigPath: './tsconfig.app.json' }),
+  {
+    name: "cargo-build",
+    buildStart: () => {
+      return new Promise((resolve, reject) => {
+        exec(
+          "cd markdown-renderer && wasm-pack build --target web --release",
+          (err, stdout, stderr) => {
+            if (err) {
+              console.log("Stdout:", stdout);
+              console.log("Stderr:", stderr);
+              reject(err);
+            } else {
+              resolve();
+            }
+          }
+        );
+      });
+    }
+  },
+
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'SolidCmark',
-      fileName: (format) => `solid-cmark.${format}.js`,
+      fileName: (format) => `solid-markdown-wasm.${format}.js`,
       formats: ['es']
     },
     rollupOptions: {
       external: ['solid-js', "solid-js/web"],
+      output: {
+        manualChunks(id) {
+          if (id.includes("markdown-renderer")) {
+            return "markdown-renderer";
+          }
+        }
+      }
     },
   },
 })
