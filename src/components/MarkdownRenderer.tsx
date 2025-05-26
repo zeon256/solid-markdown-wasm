1e7b5614d93c5971cf295ed20f6c66f018082d0d
Budi Syahiddin <me@inve.rs>
2025-05-26 23:04:34 +0800

    chore(fmt): format code with biome

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -1,5 +1,5 @@
-import { type Component, createEffect, createSignal, onMount } from 'solid-js';
 import init, { render_md } from "markdown-renderer";
+import { type Component, createEffect, createSignal, onMount } from "solid-js";
 
 export interface MarkdownRendererProps {
   markdown: string;
@@ -9,7 +9,7 @@ export interface MarkdownRendererProps {
 
 export const MarkdownRenderer: Component<MarkdownRendererProps> = (props) => {
   const { theme = "base16-ocean.dark" } = props;
-  const [renderedHtml, setRenderedHtml] = createSignal('');
+  const [renderedHtml, setRenderedHtml] = createSignal("");
   const [loadingWasm, setLoadingWasm] = createSignal(true);
   const [error, setError] = createSignal<string | null>(null);
 
@@ -29,7 +29,7 @@ export const MarkdownRenderer: Component<MarkdownRendererProps> = (props) => {
   });
 
   createEffect(() => {
-    if (!loadingWasm()) {      
+    if (!loadingWasm()) {
       const startTime = performance.now();
       const result = render_md(props.markdown, theme);
       setRenderedHtml(result);
@@ -40,12 +40,11 @@ export const MarkdownRenderer: Component<MarkdownRendererProps> = (props) => {
 
   return (
     <div>
-      {error() && <div style={{ color: 'red' }}>{error()}</div>}
+      {error() && <div style={{ color: "red" }}>{error()}</div>}
       {loadingWasm() && <p>Loading WASM...</p>}
-      <div class={props.class}>        
+      <div class={props.class}>
         <div innerHTML={renderedHtml()} />
       </div>
     </div>
   );
 };
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,53 @@
-import { defineConfig } from 'vite';
-import solid from 'vite-plugin-solid';
-import dts from 'vite-plugin-dts'
-import { resolve } from "path";
 import { exec } from "node:child_process";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid(), dts({ insertTypesEntry: true, include: ['src'], tsconfigPath: './tsconfig.app.json' }),
-  {
-    name: "cargo-build",
-    buildStart: () => {
-      return new Promise((resolve, reject) => {
-        exec(
-          "cd markdown-renderer && wasm-pack build --target web --release",
-          (err, stdout, stderr) => {
-            if (err) {
-              console.log("Stdout:", stdout);
-              console.log("Stderr:", stderr);
-              reject(err);
-            } else {
-              resolve();
-            }
-          }
-        );
-      });
-    }
-  },
-
+  plugins: [
+    solid(),
+    dts({
+      insertTypesEntry: true,
+      include: ["src"],
+      tsconfigPath: "./tsconfig.app.json",
+    }),
+    {
+      name: "cargo-build",
+      buildStart: () => {
+        return new Promise((resolve, reject) => {
+          exec(
+            "cd markdown-renderer && wasm-pack build --target web --release",
+            (err, stdout, stderr) => {
+              if (err) {
+                console.log("Stdout:", stdout);
+                console.log("Stderr:", stderr);
+                reject(err);
+              } else {
+                resolve();
+              }
+            },
+          );
+        });
+      },
+    },
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'SolidCmark',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "SolidCmark",
       fileName: (format) => `solid-markdown-wasm.${format}.js`,
-      formats: ['es']
+      formats: ["es"],
     },
     rollupOptions: {
-      external: ['solid-js', "solid-js/web"],
+      external: ["solid-js", "solid-js/web"],
       output: {
         manualChunks(id) {
           if (id.includes("markdown-renderer")) {
             return "markdown-renderer";
           }
-        }
-      }
+        },
+      },
     },
   },
-})
+});
