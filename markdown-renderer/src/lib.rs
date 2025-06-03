a4cfc18e67eb0daf4ec175906786cf43fcca2ff8
Budi Syahiddin <me@inve.rs>
2025-06-04 02:13:18 +0800

    feat: change code highlight once it reaches 10k characters to syntect

--- markdown-renderer/src/lib.rs
+++ markdown-renderer/src/lib.rs
@@ -1,4 +1,7 @@
-use comrak::{Options, Plugins, markdown_to_html_with_plugins};
+use comrak::{
+    Options, Plugins, markdown_to_html_with_plugins,
+    plugins::syntect::{SyntectAdapter, SyntectAdapterBuilder},
+};
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
 use syntastica::theme::ResolvedTheme;
@@ -10,6 +13,12 @@ mod syntastica_adapter;
 
 static GLOBAL_LANGUAGE_SET: Lazy<LanguageSetImpl> = Lazy::new(LanguageSetImpl::new);
 static GLOBAL_DARK_THEME: Lazy<ResolvedTheme> = Lazy::new(syntastica_themes::one::darker);
+static GLOBAL_LIGHT_THEME: Lazy<ResolvedTheme> = Lazy::new(syntastica_themes::one::light);
+
+const ONE_DARKER: &str = "one-darker";
+const ONE_LIGHT: &str = "one-light";
+const BASE16_OCEAN_DARK: &str = "base16-ocean.dark";
+const BASE16_OCEAN_LIGHT: &str = "base16-ocean.light";
 
 static OPTIONS: Lazy<Options> = Lazy::new(|| {
     let mut options = Options::default();
@@ -27,23 +36,74 @@ static OPTIONS: Lazy<Options> = Lazy::new(|| {
     options
 });
 
-static ADAPTERS: Lazy<HashMap<&'static str, SyntasticaAdapter>> = Lazy::new(|| {
-    let mut map = HashMap::new();
+static ADAPTERS_TREESITTER: Lazy<HashMap<&'static str, SyntasticaAdapter>> = Lazy::new(|| {
+    let mut map = HashMap::with_capacity(2);
     map.insert(
-        "base16-ocean.dark",
+        ONE_DARKER,
         SyntasticaAdapter::new(&GLOBAL_DARK_THEME, &GLOBAL_LANGUAGE_SET),
+    );
+    map.insert(
+        ONE_LIGHT,
+        SyntasticaAdapter::new(&GLOBAL_LIGHT_THEME, &GLOBAL_LANGUAGE_SET),
     );
     map
 });
 
-static PLUGINS: Lazy<Plugins<'static>> = Lazy::new(|| {
-    let mut plugins = Plugins::default();
-    plugins.render.codefence_syntax_highlighter = Some(&ADAPTERS["base16-ocean.dark"]);
+static ADAPTERS_SYNTECT: Lazy<HashMap<&'static str, SyntectAdapter>> = Lazy::new(|| {
+    let mut map = HashMap::with_capacity(2);
+    map.insert(
+        BASE16_OCEAN_DARK,
+        SyntectAdapterBuilder::new()
+            .theme(BASE16_OCEAN_DARK)
+            .build(),
+    );
+    map.insert(
+        BASE16_OCEAN_LIGHT,
+        SyntectAdapterBuilder::new()
+            .theme(BASE16_OCEAN_LIGHT)
+            .build(),
+    );
+    map
+});
 
-    plugins
+static PLUGINS_TREESITTER: Lazy<HashMap<&'static str, Plugins<'static>>> = Lazy::new(|| {
+    let mut map = HashMap::with_capacity(2);
+    let mut plugin_dark = Plugins::default();
+    plugin_dark.render.codefence_syntax_highlighter = Some(&ADAPTERS_TREESITTER[ONE_DARKER]);
+
+    let mut plugins_light = Plugins::default();
+    plugins_light.render.codefence_syntax_highlighter = Some(&ADAPTERS_TREESITTER[ONE_LIGHT]);
+
+    map.insert(ONE_DARKER, plugin_dark);
+    map.insert(ONE_LIGHT, plugins_light);
+
+    map
+});
+
+static PLUGINS_SYNTECT: Lazy<HashMap<&'static str, Plugins<'static>>> = Lazy::new(|| {
+    let mut map = HashMap::with_capacity(2);
+
+    let mut plugin_dark = Plugins::default();
+    plugin_dark.render.codefence_syntax_highlighter = Some(&ADAPTERS_SYNTECT[BASE16_OCEAN_DARK]);
+
+    let mut plugin_light = Plugins::default();
+    plugin_light.render.codefence_syntax_highlighter = Some(&ADAPTERS_SYNTECT[BASE16_OCEAN_LIGHT]);
+
+    map.insert(BASE16_OCEAN_DARK, plugin_dark);
+    map.insert(BASE16_OCEAN_LIGHT, plugin_light);
+
+    map
 });
 
 #[wasm_bindgen]
 pub fn render_md(markdown: &str, theme: &str) -> String {
-    markdown_to_html_with_plugins(markdown, &OPTIONS, &PLUGINS)
+    if markdown.len() > 10_000 {
+        if theme.contains("dark") {
+            markdown_to_html_with_plugins(markdown, &OPTIONS, &PLUGINS_SYNTECT[BASE16_OCEAN_DARK])
+        } else {
+            markdown_to_html_with_plugins(markdown, &OPTIONS, &PLUGINS_SYNTECT[BASE16_OCEAN_LIGHT])
+        }
+    } else {
+        markdown_to_html_with_plugins(markdown, &OPTIONS, &PLUGINS_TREESITTER[theme])
+    }
 }
--- markdown-renderer/src/syntastica_adapter.rs
+++ markdown-renderer/src/syntastica_adapter.rs
@@ -1,6 +1,6 @@
 use comrak::{adapters::SyntaxHighlighterAdapter, html};
 use parking_lot::Mutex;
-use std::{collections::HashMap, io};
+use std::{collections::HashMap, io, str::FromStr};
 use syntastica::{Processor, renderer::HtmlRenderer, theme::ResolvedTheme};
 use syntastica_parsers_git::{Lang, LanguageSetImpl};
 
@@ -18,6 +18,35 @@ impl SyntasticaAdapter {
     }
 }
 
+struct WrappedLang(pub Lang);
+
+impl FromStr for WrappedLang {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "bash" => Ok(WrappedLang(Lang::Bash)),
+            "c" => Ok(WrappedLang(Lang::C)),
+            "cpp" => Ok(WrappedLang(Lang::Cpp)),
+            "css" => Ok(WrappedLang(Lang::Css)),
+            "go" => Ok(WrappedLang(Lang::Go)),
+            "html" => Ok(WrappedLang(Lang::Html)),
+            "java" => Ok(WrappedLang(Lang::Java)),
+            "javascript" => Ok(WrappedLang(Lang::Javascript)),
+            "json" => Ok(WrappedLang(Lang::Json)),
+            "kotlin" => Ok(WrappedLang(Lang::Kotlin)),
+            "lua" => Ok(WrappedLang(Lang::Lua)),
+            "python" => Ok(WrappedLang(Lang::Python)),
+            "rust" => Ok(WrappedLang(Lang::Rust)),
+            "toml" => Ok(WrappedLang(Lang::Toml)),
+            "tsx" => Ok(WrappedLang(Lang::Tsx)),
+            "typescript" => Ok(WrappedLang(Lang::Typescript)),
+            "yaml" => Ok(WrappedLang(Lang::Yaml)),
+            _ => Err("Unknown language string"),
+        }
+    }
+}
+
 impl SyntaxHighlighterAdapter for SyntasticaAdapter {
     fn write_highlighted(
         &self,
@@ -25,16 +54,23 @@ impl SyntaxHighlighterAdapter for SyntasticaAdapter {
         lang_name: Option<&str>,
         code: &str,
     ) -> io::Result<()> {
-        match lang_name {
-            Some(lang_name) => {
-                let mut processor_guard = self.processor.lock();
+        let Some(name_str) = lang_name else {
+            return output.write_all(code.as_bytes());
+        };
 
-                let highlights = processor_guard.process(code, Lang::Rust).unwrap();
+        let Ok(lang) = WrappedLang::from_str(name_str) else {
+            return output.write_all(code.as_bytes());
+        };
+
+        let mut processor_guard = self.processor.lock();
+
+        match processor_guard.process(code, lang.0) {
+            Ok(highlights) => {
                 let html = syntastica::render(&highlights, &mut HtmlRenderer::new(), self.theme);
 
                 output.write_all(html.as_bytes())
             }
-            None => output.write_all(code.as_bytes()),
+            Err(_) => output.write_all(code.as_bytes()),
         }
     }
 
